94435ab75e9599ae3a53cd032747ee3372bcfed1
spadkiller <gui.jarryg@gmail.com>
2026-03-04 14:20:45 +0100

    on verra bien

--- solong/render/load_sprite.c
+++ solong/render/load_sprite.c
@@ -6,31 +6,37 @@
 /*   By: gujarry <gujarry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 19:55:56 by gujarry           #+#    #+#             */
-/*   Updated: 2026/02/26 21:25:16 by gujarry          ###   ########.fr       */
+/*   Updated: 2026/03/04 14:20:10 by gujarry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../all.h"
+#include "render.h"
+#include "../libft/libft.h"
 
 static int	load_one(t_game *g, t_img *img, char *path)
 {
 	img->ptr = mlx_xpm_file_to_image(g->mlx, path, &img->w, &img->h);
 	if (!img->ptr)
+	{
+		ft_putstr_fd("Error\nFailed to load: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd("\n", 2);
 		return (1);
+	}
 	return (0);
 }
 
 int	load_sprites(t_game *g)
 {
-	if (load_one(g, &g->sp.floor, "solong/sprite/floor.xpm"))
+	if (load_one(g, &g->sp.floor, "sprite/floor.xpm"))
 		return (1);
-	if (load_one(g, &g->sp.wall, "solong/sprite/wall.xpm"))
+	if (load_one(g, &g->sp.wall, "sprite/wall.xpm"))
 		return (1);
-	if (load_one(g, &g->sp.player, "solong/sprite/player.xpm"))
+	if (load_one(g, &g->sp.player, "sprite/player.xpm"))
 		return (1);
-	if (load_one(g, &g->sp.exit, "solong/sprite/exit.xpm"))
+	if (load_one(g, &g->sp.exit, "sprite/exit.xpm"))
 		return (1);
-	if (load_one(g, &g->sp.collect, "solong/sprite/collect.xpm"))
+	if (load_one(g, &g->sp.collect, "sprite/collect.xpm"))
 		return (1);
 	return (0);
 }
