a1429d8c8096c9a990f7135c0d18691040bc856b
spadkiller <gui.jarryg@gmail.com>
2026-03-25 14:55:22 +0100

    changement so_long

--- so_long/render/destroy_sprite.c
+++ so_long/render/destroy_sprite.c
@@ -6,7 +6,7 @@
 /*   By: gujarry <gujarry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 19:59:21 by gujarry           #+#    #+#             */
-/*   Updated: 2026/03/05 13:32:24 by gujarry          ###   ########.fr       */
+/*   Updated: 2026/03/25 14:53:48 by gujarry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,18 @@ static void	destroy_one(t_game *g, t_img *img)
 
 void	destroy_sprites(t_game *g)
 {
+	int	i;
+
 	destroy_one(g, &g->sp.floor);
 	destroy_one(g, &g->sp.wall);
 	destroy_one(g, &g->sp.player);
 	destroy_one(g, &g->sp.exit);
 	destroy_one(g, &g->sp.collect);
+	destroy_one(g, &g->sp.ghost);
+	i = 0;
+	while (i < 3)
+	{
+		destroy_one(g, &g->sp.collect_anim[i]);
+		i++;
+	}
 }
